861b5412c6f97a6dd4c0e107f896ed0d061f2abc
eva19950630 <fun910347@gmail.com>
2014-04-06 23:28:34 +0800

    用ActionListener控制我的按鈕
    Override這20個按鈕
    數字鍵(0~9)及功能鍵(+ - * / = C % +/- 1/x)
    
    有夠難...

--- src/CalculatorGUI.java
+++ src/CalculatorGUI.java
@@ -1,7 +1,6 @@
 
 import java.awt.*;
 import java.awt.event.*;
-
 import javax.swing.*;
 import javax.swing.event.*;
 
@@ -11,8 +10,14 @@ class Calculation extends JFrame {
 	private JTextField jtf;
 	private JButton jbt[];
 	private JPanel numPanel, optPanel, framePanel;
-	private
 	
+	private String now_num = "";
+	private boolean reset_num = false;
+	private int optSymbol = 0;
+	private double sol = 0;
+	private double tmp2 = 0;
+	
+	/**Constructor*/
 	public Calculation() {
 		
 		// Overall Appearance
@@ -44,7 +49,7 @@ class Calculation extends JFrame {
 		ButtonListener acl = new ButtonListener();
 		
 		// 0~9  Button
-		for (int i = 0, i <= 9, i++) {
+		for (int i = 0; i <= 9; i++) {
 			jbt[i] = new JButton(i + "");
 			jbt[i].setFont(new Font("Verdana", Font.PLAIN, 12));
 			jbt[i].setMargin(new Insets(0, 0, 0, 0));
@@ -90,7 +95,7 @@ class Calculation extends JFrame {
 		jbt[18] = new JButton("+");
 		jbt[19] = new JButton("=");
 		
-		for (int i = 12, i <= 19, i++) {
+		for (int i = 12; i <= 19; i++) {
 			optPanel.add(jbt[i]);
 			jbt[i].setForeground(Color.RED);
 			jbt[i].setFont(new Font("Verdana", Font.PLAIN, 12));
@@ -98,21 +103,150 @@ class Calculation extends JFrame {
 			jbt[i].setMargin(new Insets(0, 10, 0, 10));
 		}
 		
-		// Typesetting
+		// Composing these twenty buttons
 		framePanel.add(numPanel, BorderLayout.CENTER);
 		framePanel.add(optPanel, BorderLayout.EAST);
 		
 	}
 	
+	/**Handle these twenty buttons*/
 	private class ButtonListener implements ActionListener {
-
+		
+		@Override
 		public void actionPerformed(ActionEvent e) {
-			if 
+			if (e.getSource() == jbt[0] || e.getSource() == jbt[1] || e.getSource() == jbt[2] || e.getSource() == jbt[3] || e.getSource() == jbt[4] || e.getSource() == jbt[5] || e.getSource() == jbt[6] || e.getSource() == jbt[7] || e.getSource() == jbt[8] || e.getSource() == jbt[9]) {
+				now_num = jtf.getText();
+				if (Double.parseDouble(now_num) == 0 || reset_num) {
+					reset_num = false;
+					jtf.setText(e.getActionCommand());	
+				} else {
+					now_num = jtf.getText();
+					jtf.setText(now_num + e.getActionCommand());
+				}
+			} else if (e.getSource() == jbt[12]) {
+				double now = Double.parseDouble(jtf.getText());
+				if (optSymbol == 0) {
+					sol = now;
+					optSymbol = 4;
+					reset_num = true;
+				} else {
+					tmp2 = now;
+					sol = operation(optSymbol, sol, tmp2);
+					optSymbol = 4;
+					reset_num = true;
+				}
+				jtf.setText(sol + "");
+			} else if (e.getSource() == jbt[14]) {
+				double now = Double.parseDouble(jtf.getText());
+				if (optSymbol == 0) {
+					sol = now;
+					optSymbol = 3;
+					reset_num = true;
+				} else {
+					tmp2 = now;
+					sol = operation(optSymbol, sol, tmp2);
+					optSymbol = 3;
+					reset_num = true;
+				}
+				jtf.setText(sol + "");
+			} else if (e.getSource() == jbt[16]) {
+				double now = Double.parseDouble(jtf.getText());
+				if (optSymbol == 0) {
+					sol = now;
+					optSymbol = 2;
+					reset_num = true;
+				} else {
+					tmp2 = now;
+					sol = operation(optSymbol, sol, tmp2);
+					optSymbol = 2;
+					reset_num = true;
+				}
+				jtf.setText(sol + "");
+			} else if (e.getSource() == jbt[18]) {
+				double now = Double.parseDouble(jtf.getText());
+				if (optSymbol == 0) {
+					sol = now;
+					optSymbol = 1;
+					reset_num = true;
+				} else {
+					tmp2 = now;
+					sol = operation(optSymbol, sol, tmp2);
+					optSymbol = 1;
+					reset_num = true;
+				}
+				jtf.setText(sol + "");
+			} else if (e.getSource() == jbt[19]) {
+				double now = Double.parseDouble(jtf.getText());
+				tmp2 = now;
+				sol = operation(optSymbol, sol, tmp2);
+				reset_num = true;
+				optSymbol = 0;
+				jtf.setText(sol + "");
+			} else if (e.getSource() == jbt[13]) {
+				tmp2 = 0;
+				reset_num = true;
+				optSymbol = 0;
+				sol = 0;
+				jtf.setText("0");
+			} else if (e.getSource() == jbt[11]) {
+				String now = jtf.getText();
+				if (now.charAt(0) != '-') {
+					jtf.setText('-' + now);
+				} else {
+					jtf.setText(now.substring(1, now.length()));
+				}
+			} else if (e.getSource() == jbt[15]) {
+				double now = Double.parseDouble(jtf.getText());
+				if (optSymbol == 0) {
+					sol = now / 100;
+					optSymbol = 0;
+					reset_num = true;
+				} else {
+					tmp2 = now;
+					sol = operation(optSymbol, sol, tmp2) / 100;
+					optSymbol = 0;
+					reset_num = true;
+				}
+				jtf.setText(sol + "");
+			} else if (e.getSource() == jbt[17]) {
+				double now = Double.parseDouble(jtf.getText());
+				if (optSymbol == 0) {
+					sol = 1 / now;
+					optSymbol = 0;
+					reset_num = true;
+				} else {
+					tmp2 = now;
+					sol = 1 / operation(optSymbol, sol, tmp2);
+					optSymbol = 0;
+					reset_num = true;
+				}
+				jtf.setText(sol + "");	
+			} else if  (e.getSource() == jbt[10]) {
+				now_num = jtf.getText();
+				if (now_num.indexOf('.') == -1) {
+					jtf.setText(now_num + ".");
+				}
+			}
 			
+		}
+	
+		// Create a method "operation" to calculate + - * /
+		public double operation(int optS, double sol, double tmp2) {
+			if (optS == 1) {
+				sol = sol + tmp2;
+			} else if (optS == 2) {
+				sol = sol - tmp2;
+			} else if (optS == 3) {
+				sol = sol * tmp2;
+			} else if (optS == 4) {
+				if (tmp2 != 0.0) {
+					sol = sol / tmp2;
+				}
+			}
+			return sol;
 		}
 		
 	}
-	
 	
 }
 
@@ -120,7 +254,8 @@ class Calculation extends JFrame {
 public class CalculatorGUI {
 
 	public static void main(String[] args) {
-
+		Calculation cal = new Calculation();
+		cal.setDefaultCloseOperation(cal.EXIT_ON_CLOSE);
 	}
 
 }
